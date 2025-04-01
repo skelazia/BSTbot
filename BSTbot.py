6c6cb692c21595a024a6c9a12645b8f749e6baed
skelazia <mddlcre@gmail.com>
2025-04-02 07:45:03 +0800

    Update BSTbot.py

--- BSTbot.py
+++ BSTbot.py
@@ -8,6 +8,10 @@ from io import StringIO
 from discord.ext import commands
 from sympy import sympify
 
+intents = discord.Intents.default()
+intents.message_content = True  # Enable message content intent
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 os.environ["PORT"] = "8080"
 # Load token from environment variable
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
